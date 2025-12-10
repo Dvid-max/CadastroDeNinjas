33c984311916e1b37e5d4abcf6d205487d2b6925
Dvid-max <davidbritoal7@gmail.com>
2025-12-10 12:49:48 -0300

    feat: Adiciona rota para atualizar NinjaModel mas ainda não funcionar corretamente

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -37,9 +37,9 @@ public class NinjaController {
     }
 
     //Alterar dados dos ninjas (UPDATE)
-    @PutMapping("/alteraID")
-    public String alterarPorId(){
-        return "Alterar por ID";
+    @PutMapping("/alterar/{id}")
+    public NinjaModel alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaModel ninjaAtualizado){
+        return ninjaService.atualizarNinja(id, ninjaAtualizado);
     }
 
     //Deletar Ninja (DELETE)
